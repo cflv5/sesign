5b1f595bc3ae101ff7cda9a5f136ec66e5f6df78
cflv5 <bedirhanakcay@icloud.com>
2022-06-11 17:28:59 +0300

    initial commit

---
+++ src/main/java/tr/edu/yildiz/ce/sesign/SeSignApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.yildiz.ce.sesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeSignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeSignApplication.class, args);
+	}
+
+}
