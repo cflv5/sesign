5db5da7a7f17a0c5eda534185330a1cd02d1a923
cflv5 <bedirhanakcay@icloud.com>
2022-06-12 19:07:30 +0300

    feat: create constant class for certificate ops

---
+++ src/main/java/tr/edu/yildiz/ce/sesign/domain/constants/CertificateConstants.java
@@ -0,0 +1,11 @@
+package tr.edu.yildiz.ce.sesign.domain.constants;
+
+public final class CertificateConstants {
+    private CertificateConstants() {
+        throw new AssertionError("No instance allowed");
+    }
+
+    public static final String BC_PROVIDER = "BC";
+    public static final String KEY_ALGORITHM = "RSA";
+    public static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
+}
