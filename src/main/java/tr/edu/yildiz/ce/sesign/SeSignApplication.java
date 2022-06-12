f2744962b54df8e83a21022b0fb7ceb090bf1db2
cflv5 <bedirhanakcay@icloud.com>
2022-06-12 18:41:31 +0300

    feat: adding bc as security provider when app starts

--- src/main/java/tr/edu/yildiz/ce/sesign/SeSignApplication.java
+++ src/main/java/tr/edu/yildiz/ce/sesign/SeSignApplication.java
@@ -1,5 +1,10 @@
 package tr.edu.yildiz.ce.sesign;
 
+import java.security.Security;
+
+import javax.annotation.PostConstruct;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,9 +12,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = { "tr.edu.yildiz.ce.se.base", "tr.edu.yildiz.ce.sesign" })
 public class SeSignApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SeSignApplication.class, args);
 	}
 
+	@PostConstruct
+	void addSecurityProvider() {
+		Security.addProvider(new BouncyCastleProvider());
+	}
 }
