ece1e274c33c0ff9f192522800da5fc88af4ddf9
cflv5 <bedirhanakcay@icloud.com>
2022-06-13 14:18:25 +0300

    feat: add endpoint to sign a file using secerts

--- src/main/java/tr/edu/yildiz/ce/sesign/util/CertificateUtil.java
+++ src/main/java/tr/edu/yildiz/ce/sesign/util/CertificateUtil.java
@@ -1,9 +1,15 @@
 package tr.edu.yildiz.ce.sesign.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -29,5 +35,13 @@ public final class CertificateUtil {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.YEAR, years);
         return calendar.getTime();
+    }
+
+    public static KeyStore loadKeyStore(byte[] buff, String password)
+            throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        InputStream fis = new ByteArrayInputStream(buff);
+        ks.load(fis, password.toCharArray());
+        return ks;
     }
 }
